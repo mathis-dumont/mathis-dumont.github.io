2c5c5e2981a740cf1ae72a3b291f38e61bbb848d
mathisdumont <mathisdumont123@gmail.com>
2026-01-18 21:44:28 +0100

    fix error : Failed to load module script: Expected a JavaScript-or-Wasm module script but the server responded with a MIME type of application/octet-stream. Strict MIME type checking is enforced for module scripts per HTML spec.

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,14 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // base: '/mathis-dumont/',
   base: '/',
+  build: {
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
+  }
 })
